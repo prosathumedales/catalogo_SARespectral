92305ba321152c24509a55d02dfe6d66bf678877
Elio Campitelli <eliocampitelli@gmail.com>
2022-06-25 16:43:29 -0300

    Mueve cosas de lugar

--- procesamiento_datos.R
+++ procesamiento_datos.R
@@ -13,7 +13,7 @@ bandas_interes <- c("B1", "B2",  "B3", "B4", "B5", "B6", "B7", "B8", "B11", "B12
 bandas_interes_labels <- c("Aerosoles costeros", "Azul",  "Verde", "Rojo", "Borde rojo 1", "Borde rojo 2", "Borde rojo 3", "IR cercano", "IR medio 1", "IR medio 2")
 
 
-optico <- data.table::fread("datos/PROSAT_DeltaSuperior_Optico_S2Harmonized_UPI4.csv")
+optico <- data.table::fread("datos_raw/PROSAT_DeltaSuperior_Optico_S2Harmonized_UPI4.csv")
 
 colnames(optico)[20:21] <- c("tipo_sensor", "sensor")
 optico <- optico[, colnames(optico) %in% columnas_importantes, with = FALSE]
@@ -24,7 +24,7 @@ optico[, valor_promedio := valor_promedio * 0.01]
 
 optico[, tipo_humed := gsub(" ", "_", tipo_humed[1]), by = tipo_humed]
 
-sar <- data.table::fread("datos/PROSAT_DeltaSuperior_SAR_SENTINEL1_20220614.csv")
+sar <- data.table::fread("datos_raw/PROSAT_DeltaSuperior_SAR_SENTINEL1_20220614.csv")
 sar <- sar[, colnames(sar) %in% columnas_importantes, with = FALSE]
 
 
